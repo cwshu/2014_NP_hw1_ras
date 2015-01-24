1911f740dd3f3cbed2967a4148e831de1ea1ffb2
susu <u1240976@gmail.com>
2015-01-24 21:11:46 +0800

    same as 9ffb commit: fix variable arguments function syntax

--- io_wrapper.h
+++ io_wrapper.h
@@ -2,8 +2,8 @@
 #define __IO_WRAPPER_H__
 
 void perror_and_exit(const char* str);
-void error_print(const char* format ... );
-void error_print_and_exit(const char* format ... );
+void error_print(const char* format, ... );
+void error_print_and_exit(const char* format, ... );
 
 int write_all(int fd, const void* buf, size_t count);
 
